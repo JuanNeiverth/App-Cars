c9ed7e20bf743916be31973d480525b78184a4cb
Juan marcos Polli neiverth <polli.neiverth@gmail.com>
2023-05-25 13:42:30 -0500

    Initial Commit

---
+++ My Project Car MVVM/My_Project_Car_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Project_Car_MVVMApp.swift
+//  My Project Car MVVM
+//
+//  Created by Juan on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct My_Project_Car_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
